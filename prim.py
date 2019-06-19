63e6a947700f13b1c959be61817f001960adac22
papakuttyma <42169947+papakuttyma@users.noreply.github.com>
2019-06-19 09:13:15 +0530

    Update prim.py

--- prim.py
+++ prim.py
@@ -1,7 +1,7 @@
-num=int(input())
-if num>0:
-  for i in range(2,num):
-    if(num%i==0):
+num1=int(input())
+if num1>0:
+  for i in range(2,num1):
+    if(num1%i==0):
       print("no")
       break
   else:
